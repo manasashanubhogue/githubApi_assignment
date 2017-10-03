4b1f65d2015d530f5b4a5d01def6a0ebf306b7e8
Manasa <manasa.m@hashedin.com>
2017-10-03 12:08:04 +0530

    Display user profile

--- git_assignment/git_assignment/user_profile/models.py
+++ git_assignment/git_assignment/user_profile/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user_name = models.CharField(max_length=30)
+    login_name = models.CharField(max_length=30, unique=True)
+    login_id = models.IntegerField()
+    email = models.EmailField(max_length=254, null=True) 
--- git_assignment/git_assignment/user_profile/views.py
+++ git_assignment/git_assignment/user_profile/views.py
@@ -1,1 +1,25 @@
+
+from django.shortcuts import render, HttpResponse
+import requests
+import json
+
 # Create your views here.
+
+
+def profile(request):
+    dataList = []
+    requiredData = []
+    userData = {}
+    req = requests.get('https://api.github.com/users/mansa-maravanthe')
+    dataList.append(json.loads(req.content))
+   
+    for data in dataList:
+    	userData['login_id'] = data['id']
+    	userData['user_name'] = data['name']
+    	userData['login_name'] = data['login']
+    	userData['email'] = data['email']
+    	userData['bio'] = data['bio']
+    	userData['updated_at'] = data['updated_at']
+    	userData['created_at'] = data['created_at']
+    requiredData.append(userData)
+    return render(request, 'user_profile/profile.html', {'requiredData': requiredData})
