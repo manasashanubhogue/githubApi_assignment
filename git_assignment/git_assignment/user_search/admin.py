845576172586ad82a0f117f103832266fcd441e2
Manasa <manasa.m@hashedin.com>
2017-10-03 17:24:13 +0530

    filters and admin panel

--- git_assignment/git_assignment/user_search/admin.py
+++ git_assignment/git_assignment/user_search/admin.py
@@ -8,5 +8,8 @@ from git_assignment import settings
 class UserProfileAdmin(admin.ModelAdmin):
     class Meta:
     	model = UserProfile
+    """data_record_created_at - time of data record creation"""
+    list_display = ("login_name", "login_id", "url", "email", "created_at", "updated_at")
+    list_filter = ("created_at", "updated_at", "email", "data_record_created_at")
 
 admin.site.register(UserProfile, UserProfileAdmin)
--- git_assignment/git_assignment/user_search/models.py
+++ git_assignment/git_assignment/user_search/models.py
@@ -1,6 +1,8 @@
-from django.db import models
 import pytz
+from django.db import models
 from django.core.validators import URLValidator
+from datetime import datetime
+
 
 class UserProfile(models.Model):
     user_name = models.CharField(max_length=30, null=True, blank=True, default="None")
@@ -12,6 +14,7 @@ class UserProfile(models.Model):
     bio = models.CharField(max_length=30, null=True, blank=True, default="None")
     url = models.TextField(validators=[URLValidator()], default='')
     public_repos = models.IntegerField(default=0)
+    data_record_created_at = models.DateTimeField(auto_now=True)
     #avatar = models.ImageField(upload_to = 'pic_folder/', default = 'pic_folder/None/no-img.jpg')
 
             	
