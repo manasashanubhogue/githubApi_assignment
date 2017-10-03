324f743455d609e1abae86a4dab1de3331175bc7
Manasa <manasa.m@hashedin.com>
2017-10-03 18:52:28 +0530

    report generation using filter

--- git_assignment/git_assignment/user_search/admin.py
+++ git_assignment/git_assignment/user_search/admin.py
@@ -2,9 +2,10 @@
 
 # from django.http import HttpResponse
 from django.contrib import admin
-
 from .models import UserProfile
 from git_assignment import settings
+from django.http import HttpResponse
+import openpyxl
 
 class UserProfileAdmin(admin.ModelAdmin):
 
@@ -13,27 +14,49 @@ class UserProfileAdmin(admin.ModelAdmin):
     """data_record_created_at - time of data record creation"""
     list_display = ("login_name", "login_id", "url", "email", "created_at", "updated_at")
     list_filter = ("created_at", "email", "data_record_created_at")
-    actions =  ['download_report']
+    actions =  ['export_xlsx']
 
-    # def download_report(self, request, queryset):
-    #     """
-    #     Export data to excel sheet. Report can be generated for last year or one month or for user created today
-    #     queryset : Selected query
-    #     """
-    #     wb = export_xlsx("Reimbursement Report", queryset, [
-    #         (u"ID", 10),
-    #         (u"UserName", 30),
-    #         (u"Url", 15)
+    def export_xlsx(modeladmin, request, queryset):
+        try: 
+            from openpyxl.cell import get_column_letter
+        except ImportError:
+            from openpyxl.utils import get_column_letter
+        response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        response['Content-Disposition'] = 'attachment; filename=mymodel.xlsx'
+        wb = openpyxl.Workbook()
+        ws = wb.get_active_sheet()
+        ws.title = "MyModel"
 
-    #     ], row_map=lambda obj: [
-    #         obj.pk,
-    #         obj.login_name ,
-    #         obj.url,
-           
-    #     ])
-    #     response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-    #     response['Content-Disposition'] = 'attachment; filename=report.xlsx'
-    #     wb.save(response)
-    #     return response
+        row_num = 0
+
+        columns = [
+            (u"ID", 15),
+            (u"Name", 70),
+            (u"URL", 70),
+        ]
+
+        for col_num in xrange(len(columns)):
+            c = ws.cell(row=row_num + 1, column=col_num + 1)
+            c.value = columns[col_num][0]
+            
+            # set column width
+            ws.column_dimensions[get_column_letter(col_num+1)].width = columns[col_num][1]
+
+        for obj in queryset:
+            row_num += 1
+            row = [
+                obj.pk,
+                obj.login_name,
+                obj.url,
+            ]
+            for col_num in xrange(len(row)):
+                c = ws.cell(row=row_num + 1, column=col_num + 1)
+                c.value = row[col_num]
+        
+        wb.save(response)
+        return response
+
+    export_xlsx.short_description = u"Export XLSX"
+
 
 admin.site.register(UserProfile, UserProfileAdmin)
