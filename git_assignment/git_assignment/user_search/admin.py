d523aa8b9f4fc9ac5788e3438648b3d1166b3cf7
Manasa <manasa.m@hashedin.com>
2017-10-03 18:02:11 +0530

    Report generation and test case

--- git_assignment/git_assignment/user_search/admin.py
+++ git_assignment/git_assignment/user_search/admin.py
@@ -2,14 +2,38 @@
 
 # from django.http import HttpResponse
 from django.contrib import admin
+
 from .models import UserProfile
 from git_assignment import settings
 
 class UserProfileAdmin(admin.ModelAdmin):
+
     class Meta:
     	model = UserProfile
     """data_record_created_at - time of data record creation"""
     list_display = ("login_name", "login_id", "url", "email", "created_at", "updated_at")
-    list_filter = ("created_at", "updated_at", "email", "data_record_created_at")
+    list_filter = ("created_at", "email", "data_record_created_at")
+    actions =  ['download_report']
+
+    # def download_report(self, request, queryset):
+    #     """
+    #     Export data to excel sheet. Report can be generated for last year or one month or for user created today
+    #     queryset : Selected query
+    #     """
+    #     wb = export_xlsx("Reimbursement Report", queryset, [
+    #         (u"ID", 10),
+    #         (u"UserName", 30),
+    #         (u"Url", 15)
+
+    #     ], row_map=lambda obj: [
+    #         obj.pk,
+    #         obj.login_name ,
+    #         obj.url,
+           
+    #     ])
+    #     response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+    #     response['Content-Disposition'] = 'attachment; filename=report.xlsx'
+    #     wb.save(response)
+    #     return response
 
 admin.site.register(UserProfile, UserProfileAdmin)
--- git_assignment/git_assignment/user_search/tests.py
+++ git_assignment/git_assignment/user_search/tests.py
@@ -1,6 +1,23 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.test import TestCase
+from .views import profile
+from .models import UserProfile
 
-# Create your tests here.
+class TestUserSearch(TestCase):
+
+	def test_update_user(self):
+		self.user1 = UserProfile.objects.create(
+	            			user_name= "Test",
+	                        login_name="test1",
+	                        login_id=123,
+	                        email="None",
+	                        bio="None",
+	                        url="https://github.com/test",
+	                        public_repos=2,
+	                        created_at=timezone.now().date(),
+	                        updated_at=timezone.now().date() + datetime.timedelta(days=20)
+	        )
+		print profile("test")
+
+
