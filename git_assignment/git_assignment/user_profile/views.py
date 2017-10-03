07c4ed2cb249d9ef8850c9bf65507b27c1edc22c
Manasa <manasa.m@hashedin.com>
2017-10-03 12:41:55 +0530

    Created api to search users

--- git_assignment/git_assignment/user_profile/views.py
+++ git_assignment/git_assignment/user_profile/views.py
@@ -10,16 +10,18 @@ def profile(request):
     dataList = []
     requiredData = []
     userData = {}
-    req = requests.get('https://api.github.com/users/mansa-maravanthe')
-    dataList.append(json.loads(req.content))
+    if request.method == 'POST':
+        name = request.POST.get('user')
+        req = requests.get('https://api.github.com/users/'+ name)
+        dataList.append(json.loads(req.content))
    
-    for data in dataList:
-    	userData['login_id'] = data['id']
-    	userData['user_name'] = data['name']
-    	userData['login_name'] = data['login']
-    	userData['email'] = data['email']
-    	userData['bio'] = data['bio']
-    	userData['updated_at'] = data['updated_at']
-    	userData['created_at'] = data['created_at']
-    requiredData.append(userData)
+        for data in dataList:
+        	userData['login_id'] = data['id']
+        	userData['user_name'] = data['name']
+        	userData['login_name'] = data['login']
+        	userData['email'] = data['email']
+        	userData['bio'] = data['bio']
+        	userData['updated_at'] = data['updated_at']
+        	userData['created_at'] = data['created_at']
+        requiredData.append(userData)
     return render(request, 'user_profile/profile.html', {'requiredData': requiredData})
