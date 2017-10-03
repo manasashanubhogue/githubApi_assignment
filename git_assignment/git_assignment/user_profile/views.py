daf78e12d9917837864b257bce9141737aa25da1
Manasa <manasa.m@hashedin.com>
2017-10-03 13:01:56 +0530

    Handled generic error page

--- git_assignment/git_assignment/user_profile/views.py
+++ git_assignment/git_assignment/user_profile/views.py
@@ -1,5 +1,6 @@
 
 from django.shortcuts import render, HttpResponse
+from django.http import HttpResponseNotFound
 import requests
 import json
 
@@ -13,15 +14,18 @@ def profile(request):
     if request.method == 'POST':
         name = request.POST.get('user')
         req = requests.get('https://api.github.com/users/'+ name)
-        dataList.append(json.loads(req.content))
-   
-        for data in dataList:
-        	userData['login_id'] = data['id']
-        	userData['user_name'] = data['name']
-        	userData['login_name'] = data['login']
-        	userData['email'] = data['email']
-        	userData['bio'] = data['bio']
-        	userData['updated_at'] = data['updated_at']
-        	userData['created_at'] = data['created_at']
-        requiredData.append(userData)
+        if req.status_code ==200:
+            dataList.append(json.loads(req.content))
+            
+            for data in dataList:
+            	userData['login_id'] = data['id']
+            	userData['user_name'] = data['name']
+            	userData['login_name'] = data['login']
+            	userData['email'] = data['email']
+            	userData['bio'] = data['bio']
+            	userData['updated_at'] = data['updated_at']
+            	userData['created_at'] = data['created_at']
+            requiredData.append(userData)
+        else:
+            return render(request, 'errors/generic_error.html')
     return render(request, 'user_profile/profile.html', {'requiredData': requiredData})
