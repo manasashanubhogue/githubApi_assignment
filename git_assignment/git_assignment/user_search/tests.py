fac3e1e224fb51e74ec6dd01187778ca4572a26f
Manasa <manasa.m@hashedin.com>
2017-10-03 19:07:00 +0530

    test case update

--- git_assignment/git_assignment/user_search/tests.py
+++ git_assignment/git_assignment/user_search/tests.py
@@ -1,23 +1,37 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+import datetime
+from django.utils import timezone
 from .views import profile
 from .models import UserProfile
 
+
+
 class TestUserSearch(TestCase):
 
-	def test_update_user(self):
-		self.user1 = UserProfile.objects.create(
-	            			user_name= "Test",
-	                        login_name="test1",
-	                        login_id=123,
-	                        email="None",
-	                        bio="None",
-	                        url="https://github.com/test",
-	                        public_repos=2,
-	                        created_at=timezone.now().date(),
-	                        updated_at=timezone.now().date() + datetime.timedelta(days=20)
-	        )
-		print profile("test")
+	def setUp(self):
+        # Every test needs access to the request factory.
+    	self.factory = RequestFactory()
+        # self.user = UserProfile.objects.create(
+	       #      			user_name= "None",
+	       #                  login_name="test",
+	       #                  login_id=383316,
+	       #                  email="None",
+	       #                  bio="None",
+	       #                  url="https://github.com/test",
+	       #                  public_repos=2,
+	       #                  created_at=timezone.now().date(),
+	       #                  updated_at=timezone.now().date() + datetime.timedelta(days=20)
+	       #  )
+
+	def test_search_user(self):
+		self.factory = RequestFactory()
+		request = self.factory.post('/profile/')
+		print profile(request)
+		# response = profile(request)
+		# print response
+		
+		# self.assertEqual()
 
 
